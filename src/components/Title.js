392ab01635a9db42ee9cd19f13ffe8f5cb3584f0
BbreeSwn <suwanan.cs@hotmail.com>
2024-09-25 14:01:37 +0700

    add readme

--- src/components/Title.js
+++ src/components/Title.js
@@ -9,9 +9,15 @@ const Title = () => {
     setTheme(theme === "light" ? "dark" : "light");
   };
   return (
-    <header className={theme === "dark"? "dark" : "light"}>
+    <header className={theme === "dark" ? "dark" : "light"}>
       <span>Mode ({theme})</span>
-      <Switch onChange={toggleMode} checked={theme === "dark"} uncheckedIcon={false} checkedIcon={false} onColor={'#ffa500'}/>
+      <Switch
+        onChange={toggleMode}
+        checked={theme === "dark"}
+        uncheckedIcon={false}
+        checkedIcon={false}
+        onColor={"#ffa500"}
+      />
     </header>
   );
 };
