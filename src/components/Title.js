00a6768c651c1487b3d0bf2eb6ed481b445491a6
BbreeSwn <suwanan.cs@hotmail.com>
2024-09-25 13:11:46 +0700

    feat:create switch mode

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,10 +1,19 @@
 import Switch from "react-switch"
+import {useState} from 'react'
 
 const Title = () => {
+    const [theme , setTheme] = useState(false)
+
+const toggleMode = (checked) => {
+    setTheme(checked)
+}
   return (
     <header>
-        <span>Mode</span>
-        <Switch />
+        <span>Mode ({theme})</span>
+        <Switch
+        onChange={toggleMode}
+        checked = {theme}
+        />
     </header>
   )
 }
